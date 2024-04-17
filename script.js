348da0257abb80057814d4cddeabb3ef11afa606
Said1224nr <162983493+Said1224nr@users.noreply.github.com>
2024-04-17 16:19:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let string = prompt('String typedagi malumot kiriting')
+
+let number = prompt('Number typedagi malumot kiriting')
+
+let boolean = prompt('Boolean typedagi malumot kiriting')
+
+
+
+alert(`Siz yozgan ${string} malumoti string typedan olingan\nSiz yozgan ${number} malumoti number typedan olingan\nSiz yozgan ${boolean} malumoti boolean typedan olingan`) 
